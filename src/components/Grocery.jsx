cb6b06903212287eec29b222a4a06254afbc37a0
navaleganesh <navale.ganesh1@gmail.com>
2026-04-10 18:57:22 +0530

    changes in the header, grocery & Restarunt Menu

--- src/components/Grocery.jsx
+++ src/components/Grocery.jsx
@@ -1,6 +1,6 @@
 const Grocery = ()=>{
     return (
-        <h1 class="text-3xl font-bold underline">Grocery page!!!</h1>
+        <h1 class="text-4xl font-bold underline">Grocery page!!!</h1>
     )
 }
 
